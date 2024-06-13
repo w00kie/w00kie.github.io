9bd61f7136d63031502deddb5ba0f47f19eb1bf4
François Rejeté <francois@rejete.com>
2024-06-13 12:06:49 +0900

    add portfolio links

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,3 +1,31 @@
+import Github from "@/assets/github.svg?react";
+
+function ProjectLink({
+  href,
+  name,
+  githubLink,
+}: {
+  href: string;
+  name: string;
+  githubLink?: string;
+}) {
+  return (
+    <h3 className="inline-flex items-center text-xl font-semibold tracking-tight">
+      <a
+        href={href}
+        className="bg-gradient-to-r from-violet-400 to-purple-300 bg-clip-text text-transparent hover:underline"
+      >
+        {name}
+      </a>{" "}
+      {githubLink && (
+        <a href={githubLink} className="inline-block">
+          <Github className="mx-2 w-5 h-5 fill-current text-purple-300" />
+        </a>
+      )}
+    </h3>
+  );
+}
+
 export function Portfolio() {
   return (
     <section className="flex flex-col gap-6">
@@ -6,14 +34,11 @@ export function Portfolio() {
       </h2>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col gap-2">
-          <h3 className="text-xl font-semibold tracking-tight">
-            <a
-              href="/jpn-inheritance-tax/"
-              className="text-sky-500 hover:underline"
-            >
-              Japanese Inheritance Tax Calculator
-            </a>
-          </h3>
+          <ProjectLink
+            href="/jpn-inheritance-tax"
+            name="Japanese Inheritance Tax Calculator"
+            githubLink="https://github.com/w00kie/jpn-inheritance-tax"
+          />
           <p className="text-muted-foreground">
             A simple Japanese Inheritance Tax Calculator. It is meant to cover
             the case of a foreigner in Japan receiving an inheritance from
@@ -21,28 +46,18 @@ export function Portfolio() {
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          <h3 className="text-xl font-semibold tracking-tight">
-            <a
-              href="https://stellarid.io"
-              className="text-sky-500 hover:underline"
-            >
-              StellarID.io
-            </a>
-          </h3>
+          <ProjectLink href="https://stellarid.io" name="StellarID.io" />
           <p className="text-muted-foreground">
             A SaaS tool which lets you quickly setup federated Stellar addresses
             for your domain name without the need to install anything.
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          <h3 className="text-xl font-semibold tracking-tight">
-            <a
-              href="https://sunset.io"
-              className="text-sky-500 hover:underline"
-            >
-              Sunset.io
-            </a>
-          </h3>
+          <ProjectLink
+            href="https://sunset.io"
+            name="Sunset.io"
+            githubLink="https://github.com/w00kie/sunsetter"
+          />
           <p className="text-muted-foreground">
             A python web app running on GCP that can calculate, given a point of
             view and a point of interest (selected on a google map) on which day
